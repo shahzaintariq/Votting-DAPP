9b174f0d564765100cdb0595198ae0f248ed8031
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2020-03-02 12:22:44 -0800

    add vote func

--- contracts/votting.sol
+++ contracts/votting.sol
@@ -10,10 +10,19 @@ contract votting{
     mapping(uint => candidate) candidates;
 
     mapping(address => bool) voters;
-    
 
     function addCandidate(uint _id,string memory _name) public returns(bool){
         candidates[_id] = candidate(_id,_name,0);
+        return true;
+    }
+
+    function Vote(uint _candidateId) public returns(bool){
+        require(voters[msg.sender] != false,"you have already voted");
+        require(_candidateId > 0);
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voted++;
+
         return true;
     }
 
